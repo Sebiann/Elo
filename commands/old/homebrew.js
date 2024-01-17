cf2cb64e0ae79407936970c86fb0cdeb9620d9d1
Sebiann <code@sebiann.moe>
2024-01-17 18:40:58 +0100

    added new eslint file and linted

--- commands/old/homebrew.js
+++ commands/old/homebrew.js
@@ -1,45 +1,45 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-    name: "homebrew",
-    aliases: ["hb", "hometoothebrew"],
-    description: "Homebrew",
-    get usage() { return `${process.env.PREFIX}${this.name} <command>`; },
-    perms: ["SEND_MESSAGES"],
-    execute(message, args) {
-        let hbargs = args.shift();
-        let hbvartypes = message.guild.me.client.homebrew.filter(hbvartypes => hbvartypes.types && hbvartypes.types.includes(hbargs));
+	name: "homebrew",
+	aliases: ["hb", "hometoothebrew"],
+	description: "Homebrew",
+	get usage() { return `${process.env.PREFIX}${this.name} <command>`; },
+	perms: ["SEND_MESSAGES"],
+	execute(message, args) {
+		let hbargs = args.shift();
+		let hbvartypes = message.guild.me.client.homebrew.filter(hbvartypes => hbvartypes.types && hbvartypes.types.includes(hbargs));
 
-        if (hbvartypes.size) {
+		if (hbvartypes.size) {
 
-            let typeEmbed = new MessageEmbed()
-                .setColor(message.member.displayHexColor)
-                .setTitle(hbargs.toUpperCase() + "s")
-                .setDescription("List")
-                .setTimestamp()
-                .setFooter(`Requested by ${message.author.username}`);
+			let typeEmbed = new MessageEmbed()
+				.setColor(message.member.displayHexColor)
+				.setTitle(hbargs.toUpperCase() + "s")
+				.setDescription("List")
+				.setTimestamp()
+				.setFooter(`Requested by ${message.author.username}`);
 
-            let embedFields = hbvartypes.map(hb => {
-                return {
-                    name: hb.name,
-                    value: hb.description,
-                    inline: false
-                };
-            });
+			let embedFields = hbvartypes.map(hb => {
+				return {
+					name: hb.name,
+					value: hb.description,
+					inline: false
+				};
+			});
 
-            typeEmbed.addFields(embedFields);
+			typeEmbed.addFields(embedFields);
 
-            return message.channel.send(typeEmbed);
+			return message.channel.send(typeEmbed);
 
-        }
+		}
 
-        let hbvar = message.guild.me.client.homebrew.get(hbargs)
+		let hbvar = message.guild.me.client.homebrew.get(hbargs)
             || message.guild.me.client.homebrew.find(hbvar => hbvar.aliases && hbvar.aliases.includes(hbargs));
 
-        if (hbvar) {
-            message.channel.send(hbvar.file);
-        } else {
-            message.channel.send("I dont know that Homebrew Page");
-        }
-    }
+		if (hbvar) {
+			message.channel.send(hbvar.file);
+		} else {
+			message.channel.send("I dont know that Homebrew Page");
+		}
+	}
 };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("node:fs");
-const path = require('node:path');
+const path = require("node:path");
 
-const config = require('./config.json');
+const config = require("./config.json");
 // require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] });
@@ -9,16 +9,16 @@ const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] 
 client.commands = new Discord.Collection();
 client.homebrew = new Discord.Collection();
 
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
+		if ("data" in command && "execute" in command) {
 			client.commands.set(command.data.name, command);
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
@@ -26,8 +26,8 @@ for (const folder of commandFolders) {
 	}
 }
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
 
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
